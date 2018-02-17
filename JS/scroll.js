6c3ec0df347623d208268b96523ff126f2403549
IlyaShuv <ilya.shuv24@gmail.com>
2018-02-17 13:11:06 +0300

    Добавлена анимация при проктутке посредствам добавления скрипта scroll.js.

---
+++ JS/scroll.js
@@ -0,0 +1,11 @@
+function scrollAnimate() {
+	$(window).scroll(function() {
+	$('.mov').each(function() {
+		var imagePos = $(this).offset().top;
+		var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+200) {
+			$(this).addClass('animated fadeInLeft');
+		}
+	});
+});
+}
