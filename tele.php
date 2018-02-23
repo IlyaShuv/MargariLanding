3346eb861ddd4f7a96c6d515294e014b19ce238a
IlyaShuv <ilya.shuv24@gmail.com>
2018-02-23 14:31:47 +0300

    Доработан скрипт для отправки персональных данных (добавлен checkbox).

--- tele.php
+++ tele.php
@@ -1,27 +1,33 @@
 <?php
+if(isset($_POST['checkBox']) && 
+	$_POST['checkBox'] == 'Yes') {
+		$name = $_POST['user_name'];
+		$phone = $_POST['user_phone'];
+		$subj = $_POST['subject'];
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$subj = $_POST['subject'];
+		$token = "450563284:AAEEFsieIUdX3GDERECY93M-wFSXAt3ecpw";
+		$chat_id = "-242371000";
+		$arr = array(
+			'Имя: ' => $name,
+			'Телефон: ' => $phone,
+			'Цель занятий: ' => $subj
+		);
 
-$token = "450563284:AAEEFsieIUdX3GDERECY93M-wFSXAt3ecpw";
-$chat_id = "-242371000";
-$arr = array(
-	'Имя: ' => $name,
-	'Телефон: ' => $phone,
-	'Цель занятий: ' => $subj
-);
+		foreach ($arr as $key => $value) {
+			$txt .= "<b>".$key."</b> ".$value."%0A"; 
+		};
 
-foreach ($arr as $key => $value) {
-	$txt .= "<b>".$key."</b> ".$value."%0A"; 
-};
+		$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
+			&parse_mode=html&text={$txt}","r");
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
-	&parse_mode=html&text={$txt}","r");
-
-if ($sendToTelegram) {
-	header('Location: thank-you.html');
-} else {
-	echo "Error";	
-}
+		if ($sendToTelegram) {
+			header('Location: thank-you.html');
+		} else {
+			echo "Error";	
+		}
+ 	}
+ 	else 
+ 	{
+ 		echo "<script>alert('Необходимо дать согласие на обработку персональных данных')</script>";
+ 	}
 ?>
