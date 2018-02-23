4ca8e9aad827064e922367df4513a18385087c17
IlyaShuv <ilya.shuv24@gmail.com>
2018-02-23 11:55:11 +0300

    Исправлена опечатка в срипте отправки заявки в телеграм.

--- tele.php
+++ tele.php
@@ -16,7 +16,7 @@ foreach ($arr as $key => $value) {
 	$txt .= "<b>".$key."</b> ".$value."%0A"; 
 };
 
-$sentToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
 	&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
