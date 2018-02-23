888ff66b9c923dad1203014d1a75b71cb684ac5e
IlyaShuv <ilya.shuv24@gmail.com>
2018-02-23 16:10:05 +0300

    Доработан checkbox на форме обратной связи. JS-скрипты вынесены в отдельные файлы.

---
+++ JS/confirm.js
@@ -0,0 +1,10 @@
+function Confirm() {
+	$(document).ready(function() {
+			$('#continue').prop('disabled', true);
+			$('#agree').change(function() {
+				$('#continue').prop('disabled', function(i, val) {
+					return !val;
+				})
+			});
+		})
+}
